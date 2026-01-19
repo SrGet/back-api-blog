36123721303c846b9047ee07065fa8db86a5e64e
SrGet <116851374+SrGet@users.noreply.github.com>
2026-01-19 14:57:34 -0500

    feature/added logback configs in .properties and some loggings in services

--- src/main/java/com/api/blog/Service/FeedService.java
+++ src/main/java/com/api/blog/Service/FeedService.java
@@ -5,6 +5,7 @@ import com.api.blog.Mappers.PostMapper;
 import com.api.blog.Model.Post;
 import com.api.blog.Model.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -13,18 +14,16 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FeedService {
 
     private final PostService postService;
-    private final UserService userService;
+
 
     public List<PostResponseDTO> getUserFeed(int pageNo, int pageSize){
 
-        // Here goes all the logic to create personalized user feed
-
-        User authUser = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
         List<Post> posts = postService.getLastsPosts(PageRequest.of(pageNo-1,pageSize));
-
+        log.info("Getting lastPosts successful with pageNo: {}, pageSize: {}, Returning DTOs",pageNo-1,pageSize);
 
         return posts.stream().map(post -> postService.getPostDTO(post.getId())).toList();
 
