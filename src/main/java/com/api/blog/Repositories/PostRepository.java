7dcee70ae180207f8e144def4e734a962716f528
SrGet <116851374+SrGet@users.noreply.github.com>
2026-01-25 21:30:41 -0500

    refactor/added postsCount to UserProfileDTO

--- src/main/java/com/api/blog/Repositories/PostRepository.java
+++ src/main/java/com/api/blog/Repositories/PostRepository.java
@@ -1,6 +1,7 @@
 package com.api.blog.Repositories;
 
 import com.api.blog.Model.Post;
+import com.api.blog.Model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAllByUserId(Pageable pageable, Long userId);
+    Long countByUser(User user);
 
 
 
